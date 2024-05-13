b8a7a723568d5848dd51784bae9c5dc10d742370
Gabriel Machado <gabriel.d.machado92@gmail.com>
2024-05-13 19:56:56 -0300

    first commit

---
+++ front-end/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+        <h1>404 Not Found</h1>
+        <p>A página procurada não existe</p>
+
+    </div>
+  )
+}
+
+export default NotFound 
